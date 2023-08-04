e5424381ef937237129e9a4704661e1a9e053534
zxctl <zxctl@proton.me>
2023-08-04 15:04:39 -0500

    Grub theme update

--- Scripts/restore_sgz.sh
+++ Scripts/restore_sgz.sh
@@ -46,8 +46,8 @@ if pkg_installed grub
         fi
 
         sudo sed -i "/^GRUB_DEFAULT=/c\GRUB_DEFAULT=saved
-        /^GRUB_GFXMODE=/c\GRUB_GFXMODE=1280x1024x32,auto
-        /^#GRUB_THEME=/c\GRUB_THEME=\"/usr/share/grub/themes/pochita/theme.txt\"
+        /^GRUB_GFXMODE=/c\GRUB_GFXMODE=1920x1080x32,auto
+        /^#GRUB_THEME=/c\GRUB_THEME=\"/usr/share/grub/themes/elden_ring/theme.txt\"
         /^#GRUB_SAVEDEFAULT=true/c\GRUB_SAVEDEFAULT=true" /etc/default/grub
 
         sudo cp /boot/grub/grub.cfg /boot/grub/grub.t2.bkp
